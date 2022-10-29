e81dea37448e78fba635577fefa1dac5ed14a2c8
Al Shaharia <ifty682002@gmail.com>
2022-10-29 20:54:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
