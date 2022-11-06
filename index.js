241b8b75938976af717248e4581a15413d044807
Al Shaharia <ifty682002@gmail.com>
2022-11-06 15:39:02 +0600

    server almost done.

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxuh34q.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
   try {
+    await client.connect();
+    console.log("Database Connected");
+    const productCollection = client.db("zanjira").collection("products");
+
+    // get all product
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const products = await productCollection.find(query).toArray();
+      res.send(products);
+    });
+
+    // get one product
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+    // get product by category
+    app.get("/category/:name", async (req, res) => {
+      const name = req.params.name;
+      const query = { category: name };
+      const products = await productCollection.find(query).toArray();
+      res.send(products);
+    });
   } finally {
   }
 }
