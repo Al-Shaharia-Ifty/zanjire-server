9e1668f79fe34c29b54ce96319b89ff0ab5dd1f6
Al Shaharia <ifty682002@gmail.com>
2022-11-15 16:55:30 +0600

    temporarry done

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -8,7 +7,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const { query } = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxuh34q.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -18,7 +17,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
     console.log("Database Connected");
     const productCollection = client.db("zanjira").collection("products");
 
